e4e705b2dc0410ffb559b43d56dba04f31928562
Mathias Gaunard <mathias@gaunard.com>
2013-05-22 18:13:01 +0200

    attempt to clean up shift_right

--- modules/boost/simd/operator/include/boost/simd/operator/functions/simd/common/shift_right.hpp
+++ modules/boost/simd/operator/include/boost/simd/operator/functions/simd/common/shift_right.hpp
@@ -11,6 +11,7 @@
 
 #include <boost/simd/operator/functions/shift_right.hpp>
 #include <boost/simd/include/functions/simd/bitwise_cast.hpp>
+#include <boost/simd/include/functions/simd/splat.hpp>
 #include <boost/dispatch/meta/as_integer.hpp>
 #include <boost/mpl/logical.hpp>
 #include <boost/mpl/equal_to.hpp>
@@ -21,15 +22,9 @@ namespace boost { namespace simd { namespace ext
 {
   BOOST_SIMD_FUNCTOR_IMPLEMENTATION_IF ( boost::simd::tag::shift_right_, tag::cpu_,
                                   (A0)(A1)(X),
-                                  ( boost::mpl::and_<
-                                    boost::mpl::not_< boost::is_same<A0, A1> >
-                                  , boost::mpl::equal_to
-                                                < boost::mpl::sizeof_<A0>
-                                                , boost::mpl::sizeof_<A1>
-                                                >
-                                 >),
+                                  ( boost::mpl::not_< boost::is_same<A0, A1> > )
                                   ((simd_<arithmetic_<A0>,X>))
-                                  ((simd_<integer_<A1>,X>))
+                                  ((generic_<integer_<A1> >))
                                 )
   {
     typedef A0 result_type;
@@ -39,6 +34,20 @@ namespace boost { namespace simd { namespace ext
       return bitwise_cast<result_type>(shift_right(boost::simd::bitwise_cast<int_type>(a0), a1));
     }
   };
+
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION( boost::simd::tag::shift_right_, tag::cpu_
+                                   , (A0)(A1)(X)
+                                   , ((simd_<integer_<A0>,X>))
+                                     (scalar_<integer_<A1> >)
+                                   )
+  {
+    typedef A0 result_type;
+    BOOST_SIMD_FUNCTOR_CALL(2)
+    {
+      return shift_right(a0, splat< simd::native<A1, X> >(a1));
+    }
+  };
+
 } } }
 
 #endif
